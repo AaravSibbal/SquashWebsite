b0ceb7cfe86ba23a8d23ce1d70cf3dbcdc88a00d
AaravSibbal <106462208+AaravSibbal@users.noreply.github.com>
2024-11-06 21:01:14 -0500

    search.js

---
+++ ui/static/js/search.js
@@ -0,0 +1,25 @@
+const searchInput = document.getElementById('player-search')
+
+// Event listener for the search input
+document.getElementById("searchInput").addEventListener("input", function (e) {
+  const searchTerm = e.target.value.toLowerCase();
+  const filteredPlayers = players.filter(player =>
+    player.name.toLowerCase().includes(searchTerm)
+  );
+  
+  displayResults(filteredPlayers);
+});
+
+// Function to display the filtered results
+/**
+ * 
+ * @param {Player[]} players 
+ */
+function displayResults(players) {
+  rankingTableBody.innerHTML = ""; // Clear previous results
+
+  players.forEach(player => {
+    row = player.toHTMLTableRow()
+    rankingTableBody.appendChild(playerElement);
+  });
+}
