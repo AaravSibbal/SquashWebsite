7492ccfc4c3a993ae3854545a09842d51e78c4d9
AaravSibbal <106462208+AaravSibbal@users.noreply.github.com>
2024-11-06 20:43:09 -0500

    player matches handler

--- server/handler.go
+++ server/handler.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	psql "github.com/AaravSibbal/SquashWebsite/pkg/sql"
 )
@@ -41,7 +42,10 @@ func (app *application) playerRankings(w http.ResponseWriter, r *http.Request){
 
 func (app *application) playerStat(w http.ResponseWriter, r *http.Request){
 	name := r.URL.Query().Get(":name")
-	
+	if name == "" {
+		app.clientError(w, http.StatusBadRequest)
+	}	
+
 	player, err := psql.GetPlayerData(app.db, app.ctx, name)
 	if err != nil {
 		app.serverError(w, err)
@@ -57,9 +61,51 @@ func (app *application) playerStat(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) playerGraph(w http.ResponseWriter, r *http.Request){
-		
+	name := r.URL.Query().Get(":name")
+	if name == ""{
+		app.clientError(w, http.StatusBadRequest)
+	}
+
+	matches, err := psql.GetPlayerMatches(app.db, app.ctx, name, 0)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	// TODO: GET THE GOECHARTS WORKING
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("a;sidf"))
 }
 
 func (app *application) playerMatches(w http.ResponseWriter, r *http.Request){
-	
+	var pageInt int
+	name := r.URL.Query().Get(":name")
+	if name == "" {
+		app.clientError(w, http.StatusBadRequest)
+	}
+
+	pageStr := r.URL.Query().Get("record")
+	if pageStr == "" {
+		pageInt = 0
+	} else {
+		page, err := strconv.Atoi(pageStr)
+		if err != nil {
+			pageInt = 0
+		} else{
+			pageInt = page
+		}
+	}
+
+	matches, err := psql.GetPlayerMatches(app.db, app.ctx, name, pageInt)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	matchesJson, err := json.Marshal(matches)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(matchesJson)
 }
